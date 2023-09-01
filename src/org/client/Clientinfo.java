306fb95894d916637a02ea78319124067f789856
dhaya1407 <dhayanithi1407@gmail.com>
2023-09-01 12:35:57 +0530

    new

---
+++ src/org/client/Clientinfo.java
@@ -0,0 +1,22 @@
+package org.client;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+public class Clientinfo {
+
+
+		private void student() {
+			System.out.println("dhaya");
+
+		}
+		public static void main(String[] args) {
+			
+			Clientinfo a = new Clientinfo();
+			
+			a.student();
+		}
+	
+			
+	
+}
